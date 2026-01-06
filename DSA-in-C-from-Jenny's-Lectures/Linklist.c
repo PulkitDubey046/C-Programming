2b9540f338d637e6ec01126a2cc38a497f1f1e4d
Pulkit Dubey <46dubey@gmail.com>
2026-01-06 06:55:06 +0530

    Add display and deletion functions for linked list

--- DSA-in-C-from-Jenny's-Lectures/Linklist.c
+++ DSA-in-C-from-Jenny's-Lectures/Linklist.c
@@ -9,6 +9,27 @@ struct Node
 
 struct Node *head = NULL;
 
+// display list
+void display_linked_list()
+{
+    if (head == NULL)
+    {
+        printf("List is empty.\n");
+        return;
+    }
+
+    struct Node *temp = head;
+    printf("Linked List: ");
+
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("\n");
+}
+
 // insert at end
 void insert_linked_list()
 {
@@ -51,7 +72,8 @@ void insert_start()
 }
 
 // insert at given location
-void insert_loc(){
+void insert_loc()
+{
     int l;
     printf("Enter the position: ");
     scanf("%d", &l);
@@ -60,11 +82,12 @@ void insert_loc(){
     printf("Enter the value: ");
     scanf("%d", &value);
 
-    struct Node *newNode=(struct Node *) malloc(sizeof(struct Node));
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->data = value;
     newNode->next = NULL;
 
-    if (l == 0) {
+    if (l == 0)
+    {
         newNode->next = head;
         head = newNode;
         printf("Inserted successfully.\n");
@@ -72,12 +95,14 @@ void insert_loc(){
     }
 
     struct Node *temp = head;
-    
-    for (int i = 0; i < l - 1 && temp != NULL; i++) {
+
+    for (int i = 0; i < l - 1 && temp != NULL; i++)
+    {
         temp = temp->next;
     }
 
-    if (temp == NULL) {
+    if (temp == NULL)
+    {
         printf("Position out of range.\n");
         free(newNode);
         return;
@@ -106,14 +131,17 @@ void linkedlist_insertion()
         {
         case 1:
             insert_linked_list();
+            display_linked_list();
             printf("\n");
             break;
         case 2:
             insert_start();
+            display_linked_list();
             printf("\n");
             break;
         case 3:
             insert_loc();
+            display_linked_list();
             printf("\n");
             break;
         case 4:
@@ -126,29 +154,38 @@ void linkedlist_insertion()
     }
 }
 
-// display list
-void display_linked_list()
+// delete from start
+void delete_from_start()
 {
     if (head == NULL)
     {
-        printf("List is empty.\n");
+        printf("List is empty. Nothing to delete.\n");
         return;
     }
-
     struct Node *temp = head;
-    printf("Linked List: ");
+    head = head->next;
+    free(temp);
+    printf("Deleted successfully.\n");
+}
 
-    while (temp != NULL)
-    {
-        printf("%d ", temp->data);
+void delete_from_end(){ 
+    if(head==NULL){
+        printf("List is empty. Nothing to delete.\n");
+        return;
+    }
+    struct Node *temp, *prev;
+    temp=head;
+    while(temp->next!=NULL){
+        prev = temp;
         temp = temp->next;
     }
-
-    printf("\n");
+    prev->next=NULL;
+    free(temp);
+    printf("Deleted successfully.\n");
 }
 
 // delete by value
-void delete_linked_list()
+void delete_by_value()
 {
     if (head == NULL)
     {
@@ -190,6 +227,46 @@ void delete_linked_list()
     printf("Deleted %d.\n", value);
 }
 
+// Deletion of linkedlist
+void linkedlist_deletion()
+{
+    while (1)
+    {
+        int choice;
+        printf("\nHow to delete:\n");
+        printf("1. Delete from end\n");
+        printf("2. Delete from beggining\n");
+        printf("3. Delete given value\n");
+        printf("4. Exit\n");
+        printf("Enter your choice:");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            delete_from_end();
+            display_linked_list();
+            printf("\n");
+            break;
+        case 2:
+            delete_from_start();
+            display_linked_list();
+            printf("\n");
+            break;
+        case 3:
+            delete_by_value();
+            display_linked_list();
+            printf("\n");
+            break;
+        case 4:
+            return;
+            break;
+        default:
+            printf("Invalid choice! Try again.");
+            break;
+        }
+    }
+}
+
 int main(void)
 {
     while (1)
@@ -214,7 +291,7 @@ int main(void)
             display_linked_list();
             break;
         case 3:
-            delete_linked_list();
+            linkedlist_deletion();
             break;
         case 4:
             exit(0);
